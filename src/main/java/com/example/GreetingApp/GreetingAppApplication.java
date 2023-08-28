f5843f42d67073dc0a6d1f57879c445ce5bdb495
Your Name <saifi.mujeeb2@gmail.com>
2023-08-28 11:36:36 +0530

    Added: Solution for passing JSON message for diffrent HTTP methods

--- src/main/java/com/example/GreetingApp/GreetingAppApplication.java
+++ src/main/java/com/example/GreetingApp/GreetingAppApplication.java
@@ -1,4 +1,4 @@
-package com.example.GreetingApp;
+package com.example.greetingapp;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
---
+++ src/main/java/com/example/GreetingApp/model/User.java
@@ -0,0 +1,20 @@
+package com.example.greetingapp.model;
+
+public class User {
+    private String message;
+
+    public User() {
+    }
+
+    public User(String message) {
+        this.message = message;
+    }
+
+    public String getMessage() {
+        return message;
+    }
+
+    public void setMessage(String message) {
+        this.message = message;
+    }
+}
